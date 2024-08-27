c30ba21c1ac3f47c725f860ddfec528d0406f1bf
Jen Reeve <jennifer.reeve@netvalue.nz>
2024-08-27 02:30:09 +0000

    improving plot formatting

--- gdp_plots.py
+++ gdp_plots.py
@@ -1,8 +1,16 @@
 import pandas
 import matplotlib.pyplot as plt
 
-data = pandas.read_csv('data/gapminder_gdp_oceania.csv', index_col = 'country').T
+filename = 'data/gapminder_gdp_oceania.csv'
 
-ax = data.plot()
+data = pandas.read_csv(filename, index_col = 'country').T
+
+ax = data.plot(title = filename)
+
+ax.set_xlabel("Year")
+ax.set_ylabel("GDP Per Capita")
+
+ax.set_xticks(range(len(data.index)))
+ax.set_xticklabels(data.index, rotation = 45)
 
 plt.show()
